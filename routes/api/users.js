a13d256671c9ca35f8e4a82c39e449269de9c9dc
sabitijoelit config --global user.name sabitijoel <sabitijoel@gmail.com>
2020-07-22 15:44:20 +0300

    update of login and register

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,160 +1,174 @@
-
-import bcrypt from 'bcryptjs';
-import express from 'express';
+import bcrypt from "bcryptjs";
+import express from "express";
 import jwt from "jsonwebtoken";
-import dotenv from 'dotenv';
-import { autoPassword } from '../../validation/autoGenPass';
-const passport = require('passport')
+import dotenv from "dotenv";
+import { autoPassword } from "../../validation/autoGenPass";
+const passport = require("passport");
 
 dotenv.config();
 
 const router = express.Router();
 
 // Load Input validations
-const validateRegisterInput = require('../../validation/register');
+const validateRegisterInput = require("../../validation/register");
 
-// Check for validation errors
-// if(!isValid){
-//     return res.status(400).json(errors);
-// }
+const User = require("../../models/User");
 
-const User = require('../../models/User');
-
-router.get('/', (req, res) => res.json({msg: "Users works"}));
+router.get("/", (req, res) => res.json({ msg: "Users works" }));
 
 // User registration
-router.post('/register', (req, res) => {
-    const { errors, isValid } = validateRegisterInput(req.body);
+router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
 
-    User.findOne({email: req.body.email})
-        .then(user =>{
-            if(user) return res.status(400).json({email: "Email already exists"});
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                address: req.body.address,
-                role: req.body.role,
-                contact: req.body.contact,
-                password: req.body.password
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) return res.status(400).json({ email: "Email already exists" });
+
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      address: req.body.address,
+      role: req.body.role,
+      contact: req.body.contact,
+      password: req.body.password,
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+          .save()
+          .then((user) => res.json(user))
+          .catch((err) => console.log(err));
+      });
+    });
+  });
+});
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then((user) => {
+    // Check for user
+    if (!user) return res.status(404).json({ User: "User not found" });
+
+    // Check password
+    bcrypt.compare(password, user.password).then((isMatch) => {
+      if (isMatch) {
+        // jwt payload
+        const payload = {
+          id: user.id,
+          name: user.name,
+        };
+        // Generate token
+        jwt.sign(
+          payload,
+          process.env.secret,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: `Bearer ${token}`,
             });
-            
-            bcrypt.genSalt(10, (err, salt)=>{
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if(err) throw err;
-                    newUser.password = hash;
-                    newUser.save()
-                        .then(user => res.json(user))
-                        .catch(err => console.log(err))
-                    });
-                });
-            });
-        });
-
-router.post('/login', (req, res)=>{
-    const email = req.body.email;
-    const password = req.body.password;
-
-    User.findOne({email})
-        .then(user => {
-            // Check for user
-            if(!user) return res.status(404).json({User: "User not found"});
-
-            // Check password
-            bcrypt.compare(password, user.password)
-                .then(isMatch => {
-                    if(isMatch) {
-                        // jwt payload
-                        const payload = {
-                            id: user.id, name: user.name,
-                        }
-                        // Generate token
-                        jwt.sign(payload, process.env.secret, {expiresIn: 3600}, (err, token)=>{
-                            res.json({
-                                success: true,
-                                token: `Bearer ${token}`
-                            })
-                        });
-                    } else{
-                        return res.status(404).json({User: "User not found"});
-                    }
-                });
-        });
+          }
+        );
+      } else {
+        return res.status(404).json({ User: "User not found" });
+      }
+    });
+  });
 });
 
 // Protected route
-router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
     res.json({
-        id: req.user.id,
-        name: req.user.name,
-        email: req.user.email,
-        address: req.user.address,
-        role: req.user.role,
-        contact: req.user.contact,
-        date: req.user.date
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+      address: req.user.address,
+      role: req.user.role,
+      contact: req.user.contact,
+      date: req.user.date,
     });
-});
+  }
+);
 
 // Protected route, Adding a Client
-router.post('/client', passport.authenticate('jwt', { session: false }), (req, res) => {
-    if (req.user.role == !'admin') return res.json({msg: 'Not admin'})
-    let role =  'client';
+router.post(
+  "/client",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if (req.user.role == !"admin") return res.json({ msg: "Not admin" });
+    let role = "client";
 
-    User.findOne({email: req.body.email})
-        .then(user =>{
-            if(user) return res.status(400).json({email: "Email already exists"});
-            
-        const newUser = new User({
-            name: req.body.name,
-            email: req.body.email,
-            address: req.body.address,
-            gender: req.body.gender,
-            role,
-            contact: req.body.contact,
-            password: autoPassword
-        });
-    
-        bcrypt.genSalt(10, (err, salt)=>{
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-                if(err) throw err;
-                newUser.password = hash;
-                newUser.save()
-                    .then(user => res.json(user))
-                    .catch(err => console.log(err))
-                });
-            });
-        });
-    })
+    User.findOne({ email: req.body.email }).then((user) => {
+      if (user) return res.status(400).json({ email: "Email already exists" });
 
-    // Protected route, Adding a Supplier
-    router.post('/supplier', passport.authenticate('jwt', { session: false }), (req, res) => {
-        if (req.user.role == !'admin') return res.json({msg: 'Not admin'})
-        let role =  'supplier';
-        
-        User.findOne({email: req.body.email})
-            .then(user =>{
-                if(user) return res.status(400).json({email: "Email already exists"});
-                
-            const newUser = new User({
-                name: req.body.name,
-                email: req.body.email,
-                address: req.body.address,
-                gender: req.body.gender,
-                role,
-                contact: req.body.contact,
-                password: autoPassword
-            });
-            
-            bcrypt.genSalt(10, (err, salt)=>{
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
-                    if(err) throw err;
-                    newUser.password = hash;
-                    newUser.save()
-                        .then(user => res.json(user))
-                        .catch(err => console.log(err))
-                    });
-                });
-            });
-        })
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address,
+        gender: req.body.gender,
+        role,
+        contact: req.body.contact,
+        password: autoPassword,
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
+    });
+  }
+);
+
+// Protected route, Adding a Supplier
+router.post(
+  "/supplier",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if (req.user.role == !"admin") return res.json({ msg: "Not admin" });
+    let role = "supplier";
+
+    User.findOne({ email: req.body.email }).then((user) => {
+      if (user) return res.status(400).json({ email: "Email already exists" });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        address: req.body.address,
+        gender: req.body.gender,
+        role,
+        contact: req.body.contact,
+        password: autoPassword,
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
+    });
+  }
+);
 
 module.exports = router;
--- validation/register.js
+++ validation/register.js
@@ -1,14 +1,69 @@
-import validator from 'validator';
-import isEmpty from './is-empty';
+const validator = require("validator");
+const isEmpty = require("./is-empty");
+module.exports = function validateRegisterInput(data) {
+  let errors = {};
 
-module.exports = function validatorRegisterInput(data){
-    let errors = {};
+  data.name = !isEmpty(data.name) ? data.name : "";
+  data.email = !isEmpty(data.email) ? data.email : "";
+  data.password = !isEmpty(data.password) ? data.password : "";
+  data.password2 = !isEmpty(data.password2) ? data.password2 : "";
+  data.address = !isEmpty(data.address) ? data.address : "";
+  data.role = !isEmpty(data.role) ? data.role : "";
+  data.contact = !isEmpty(data.contact) ? data.contact : "";
 
-    if (!validator.isLength(data.name, {min: 3, max: 15})){
-        errors.name = 'Name must be between 3 and 15 characters'
+  if (!validator.isLength(data.name, { min: 3, max: 30 })) {
+    errors.name = "name must be between 3 to 30 characters";
+  }
+
+  if (validator.isEmpty(data.name)) {
+    errors.name = "name field is required";
+  }
+
+  if (validator.isEmpty(data.email)) {
+    errors.email = "email field is required";
+  }
+
+  if (!validator.isEmail(data.email)) {
+    errors.email = "email is invalid";
+  }
+
+  if (!validator.isLength(data.password, { min: 5, max: 30 })) {
+    errors.password = "password must be at least 5 characters";
+  }
+
+  if (validator.isEmpty(data.password)) {
+    errors.password = "insert your account password";
+  }
+
+  if (validator.isEmpty(data.address)) {
+    errors.address = "please insert an address";
+
+    if (validator.isEmpty(data.role)) {
+      errors.role = "please insert the role of this new entry";
     }
-    return {
-        errors,
-        isValid: isEmpty(errors)
-    }
-}
+  }
+
+  if (validator.isEmpty(data.password2)) {
+    errors.password2 = "you must confirm your password";
+  }
+
+  if (!validator.equals(data.password, data.password2)) {
+    errors.password2 = "password mismatch";
+  }
+
+  if (validator.isEmpty(data.contact)) {
+    errors.contact = "insert the contact for this entry";
+  }
+
+  if (!validator.isNumeric(data.contact)) {
+    errors.contact = "the contact must be a phone number";
+  }
+
+  if (!validator.isLength(data.contact, { min: 10, max: 12 })) {
+    errors.contact = "contact must be at least 10 digits long";
+  }
+  return {
+    errors,
+    isValid: isEmpty(errors),
+  };
+};
