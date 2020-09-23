19e59e76634ab9685a2514e1d1b25f6ad536ad49
joelethan <joelkatsme@gmail.com>
2020-09-23 11:47:34 +0300

    fix heroku cors

--- server.js
+++ server.js
@@ -11,7 +11,10 @@ const measurement = require('./routes/api/measurement');
 
 const app = express();
 
-app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+  });
 
 // Body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
